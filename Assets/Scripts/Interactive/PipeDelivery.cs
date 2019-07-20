49ca775864fdcf36a6917bb090cbb5fd6fb9c2dd
kirito777 <949420063@qq.com>
2019-07-20 17:40:06 +0800

    脚本：管道、隐藏陷阱、碰就死、可破坏的墙

---
+++ Assets/Scripts/Interactive/PipeDelivery.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeDelivery : BaseInteractive
+{
+    public GameObject other_pipe;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public override void InteractiveLogic(Transform player)
+    {
+        player.position = other_pipe.transform.position;
+    }
+}
