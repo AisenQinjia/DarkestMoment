8e5c215f98163affe1443136e1a1d16eb7ab636a
艾森 <aisen@shangyoo.com>
2019-07-21 19:36:46 +0800

    modify: 修改bodycollider大小（更小），现在和enemy有任何物理接触即死亡。

---
+++ Assets/Scripts/Enemy/EnemyCollision.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCollision : MonoBehaviour
+{
+    BaseRoleController enemy;
+    void Start()
+    {
+        enemy = transform.GetComponentInParent<BaseRoleController>();
+        if (enemy == null) Debug.LogError("enemy not found!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag(GameDefine.PlayerTag))
+        {
+            Debug.Log("PlayerTag");
+            enemy.Attack();
+            other.gameObject.GetComponent<BaseRoleController>().OnDead();
+        }
+    }
+}
