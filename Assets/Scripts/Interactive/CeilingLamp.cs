7e96bcb556243596e8d7fe057467b542433510dd
kirito777 <949420063@qq.com>
2019-07-21 20:23:15 +0800

    更新吊灯脚本，只有掉下来的时候碰到才会死

--- Assets/Scripts/Interactive/CeilingLamp.cs
+++ Assets/Scripts/Interactive/CeilingLamp.cs
@@ -6,6 +6,7 @@ public class CeilingLamp : BaseInteractive
 {
     Rigidbody2D ceiling_lamp;
     string check_tag;
+    Rigidbody2D check_velocity;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +20,24 @@ public class CeilingLamp : BaseInteractive
         ceiling_lamp.gravityScale = 1;
     }
 
-    
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        check_velocity = other.rigidbody;
+        check_tag = other.gameObject.tag;
+        if (check_velocity.velocity[1] > 0)
+        {
+            if (check_tag == GameDefine.PlayerTag)
+            {
+                Debug.Log("Player ceiling touch die");
+                BaseRoleController control_dead = other.collider.GetComponent<BaseRoleController>();
+                control_dead.OnDead();
+            }
+            if (check_tag == GameDefine.EnemyTag)
+            {
+                Debug.Log("Enemy ceiling touch die");
+                BaseRoleController control_dead = other.collider.GetComponentInParent<BaseRoleController>();
+                control_dead.OnDead();
+            }
+        }
+    }
 }
