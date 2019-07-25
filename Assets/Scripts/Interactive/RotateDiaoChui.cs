378b0fb57f1754b6628fd38378840b02335841df
艾森 <aisen@shangyoo.com>
2019-07-25 17:29:48 +0800

    Add: 改个锤子！

--- Assets/Scripts/Interactive/RotateDiaoChui.cs
+++ Assets/Scripts/Interactive/RotateDiaoChui.cs
@@ -4,28 +4,29 @@ using UnityEngine;
 
 public class RotateDiaoChui : MonoBehaviour
 {
-    public int interval = 2;
-    private float timer = 1;
+    public float speed = 1;
+    private float timer ;
+    float time;
+    private int coeffient;
     public float angle = 100;
-    private float dir = 1;
+    private float dir = -1;
 
     void Start()
     {
         transform.parent.Rotate(new Vector3(0, 0, angle * 0.5f));
+        coeffient = 1;
+        time = 1 / speed;
     }
 
     // Update is called once per frame
     void Update()
     {
         timer += Time.deltaTime;
-        if ((int)timer % interval == 0)
+        if(timer > coeffient* time)
         {
-            dir = 1;
+            dir = -dir;
+            coeffient++;
         }
-        else
-        {
-            dir = -1;
-        }
-        this.transform.parent.Rotate(new Vector3(0, 0, 1), dir * angle * Time.deltaTime);
+        this.transform.parent.Rotate(new Vector3(0, 0, 1), speed * dir * angle * Time.deltaTime);
     }
 }
